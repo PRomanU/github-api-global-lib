5e8d60a6c0ac14aa49c46bbd00d805b3b9966feb
PRomanU <doodledooags@mail.com>
2025-05-23 18:33:32 +0400

    cdxgen-java11

---
+++ vars/cdxgenJava17.groovy
@@ -0,0 +1,8 @@
+// vars/cdxgenJava17.groovy
+def call(Map args = [:], Closure body) {
+  // считываем шаблон из resources
+  String yaml = libraryResource('cdxgen-java17.yaml')
+  podTemplate(yaml: yaml) {
+    body()
+  }
+}
