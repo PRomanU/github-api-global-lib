4e8edc11291705577a5ee30c4f45abe7c22cbeea
PRomanU <46927282+PRomanU@users.noreply.github.com>
2025-05-23 19:01:22 +0400

    Update cdxgenJava17.groovy

--- vars/cdxgenJava17.groovy
+++ vars/cdxgenJava17.groovy
@@ -1,8 +1,13 @@
 // vars/cdxgenJava17.groovy
-def call(Map args = [:], Closure body) {
-  // считываем шаблон из resources
-  String yaml = libraryResource('cdxgen-java17.yaml')
-  podTemplate(yaml: yaml) {
-    body()
-  }
+// def call(Map args = [:], Closure body) {
+//   // считываем шаблон из resources
+//   String yaml = libraryResource('cdxgen-java17.yaml')
+//   podTemplate(yaml: yaml) {
+//     body()
+//   }
+// }
+
+
+def call() {
+  return libraryResource('cdxgen-java17.yaml')
 }
