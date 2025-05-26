b6259b15740b1ff3bed3fb636b5d78a0b53a88bb
PRomanU <doodledooags@mail.com>
2025-05-26 14:50:44 +0400

    without resources

--- vars/cdxgenSwift.groovy
+++ vars/cdxgenSwift.groovy
@@ -1,3 +1,37 @@
 def call() {
-  return libraryResource('cdxgen-swift.yaml')
+  return """
+apiVersion: v1
+kind: Pod
+metadata:
+  name: cdxgen-swift
+spec:
+  restartPolicy: Never
+  containers:
+    - name: cdxgen-swift
+      image: ghcr.io/cyclonedx/cdxgen-debian-swift:v11
+      command:
+        - cat
+      tty: true
+      env:
+        - name: CDXGEN_DEBUG_MODE
+          value: "verbose"
+        - name: WS_NAME
+          value: "${WS_NAME}"
+      resources:
+        requests:
+          cpu: "500m"
+          memory: "1Gi"
+          ephemeral-storage: "2Gi"
+        limits:
+          cpu: "2000m"
+          memory: "2Gi"
+          ephemeral-storage: "6Gi"
+      volumeMounts:
+        - name: jenkins-pvc
+          mountPath: /app
+  volumes:
+    - name: jenkins-pvc
+      persistentVolumeClaim:
+        claimName: longhorn-jenkins-pv-claim  
+  """
 }
--- vars/syftJar.groovy
+++ vars/syftJar.groovy
@@ -1,3 +1,31 @@
 def call() {
-  return libraryResource('syft-jar.yaml')
+  return """
+apiVersion: v1
+kind: Pod
+spec:  
+  securityContext:
+    fsGroup: 1000  
+    runAsUser: 1000
+    runAsGroup: 1000
+  containers:
+    - name: syft 
+      image: xoroma/syft:test-a
+      resources:
+        limits:
+          memory: "4Gi" 
+          cpu: "3000m"
+        requests:
+          memory: "2Gi" 
+          cpu: "2000m"                                  
+      command:
+        - cat
+      tty: true
+      volumeMounts:
+        - name: syft-data
+          mountPath: /src
+  volumes:
+    - name: syft-data
+      persistentVolumeClaim:
+        claimName: longhorn-jenkins-pv-claim  
+  """
 }
